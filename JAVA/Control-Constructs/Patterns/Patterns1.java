5c7c73abda165074e8125bb09859290c36b2b6aa
Pradhum80 <kumarroney154@gmail.com>
2026-04-12 19:08:10 +0530

    Patterns : 1

---
+++ JAVA/Control-Constructs/Patterns/Patterns1.java
@@ -0,0 +1,10 @@
+package Patterns;
+
+public class Patterns1 {
+    public static void main(String[] args) {
+        for(int i = 0; i <5; i++)
+        {
+            System.out.println("*");
+        }
+    }
+}
