22b4fcf05c687660da4d330f3d88b619b05b3833
Pradhum80 <kumarroney154@gmail.com>
2026-04-26 09:23:58 +0530

    Collection : List

---
+++ JAVA/COLLECTION-FRAMEWORK/LIST/ArrayList1.java
@@ -0,0 +1,18 @@
+// package JAVA.COLLECTION-FRAMEWORK.LIST;
+
+import java.util.ArrayList;
+public class ArrayList1 {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> al = new ArrayList<>();
+        // Add Values....
+
+        al.add(10);
+        al.add(20);
+        al.add(30);
+
+        System.out.println(al);
+    }
+    
+}
